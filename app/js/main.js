e10a25a378f72c2db5c6d59a2fadbaabe1562a61
V-Svistunova <wika1706@gmail.com>
2021-12-13 02:18:34 +0300

    feat: add blog-page

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,13 @@ $(function () {
     slidesToScroll: 10,
   });
 
+  $('.article-slider__box').slick({    
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="./images/arrow-slider-left.svg" alt="arrow for slider to left"></button>' ,
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="./images/arrow-slider-right.svg" alt="arrow for slider to right"></button>' ,
+  });
+
+  
+
   var mixer = mixitup('.gallery__inner' , {
     load: {
       filter: '.living'
