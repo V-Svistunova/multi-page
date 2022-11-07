ebc2b8f3b706d14b5a1fa814bfa2a1610d7193e3
V-Svistunova <wika1706@gmail.com>
2022-11-07 19:36:32 +0300

    feat: import libs and cdn libs

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import * as flsFunctions from "./modules/functions.js"
+import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp(); // для коректной работы пребразования файлов в webp формат
 
